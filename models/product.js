18ce702da45f854e69fc34db3aa01f7cdafc3c5f
FirasKedidi <kedidi.firas1@gmail.com>
2022-06-08 12:02:06 +0100

    first commit

---
+++ models/product.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const internal = require('stream')
+const Schema = mongoose.Schema
+
+const productSchema = new Schema({
+    amountAvailable: {
+        type: Number,
+        required: true
+    },
+    cost: {
+        type: Number,
+        required: true
+    },
+    productName: {
+        type: String,
+        required: true
+    },
+    sellerId: {
+        type: String,
+        required: true
+    }
+
+})
+
+const Product = mongoose.model('Product', productSchema)
+module.exports = Product
